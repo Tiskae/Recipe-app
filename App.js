aeea3ff1756570ba8faf8d182e12750be0bf6470
user <user@users-MacBook-Pro.local>
2021-12-23 11:01:18 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+import CategoriesScreen from "./screens/CategoriesScreen";
+
+export default function App() {
+  return (
+    <View style={{ flex: 1 }}>
+      <CategoriesScreen />
+    </View>
+  );
+}
